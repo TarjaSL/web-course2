13b6dd82d7559c5f5a43f9e673b0bbc159101d4c
Tarja <svetozar-vk@mail.ru>
2016-03-07 22:35:14 +0300

    New task 2

---
+++ hello.py
@@ -0,0 +1,11 @@
+def wsgi_application(environ, start_response):
+status = '200 OK'
+headers = [
+('Content-Type', 'text/plain')
+]
+S = QUERY_STRING
+n = S.rfind('?') + 1	
+body = S[n:len(S)]
+body = body.replace('&', '\n')
+start_response(status, headers )
+return [ body ]
