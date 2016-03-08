e61a434a2cb271cb3691598f8c05e85fe6f1a54a
Tarja <svetozar-vk@mail.ru>
2016-03-08 21:24:01 +0300

    New task 4

--- hello.py
+++ hello.py
@@ -1,11 +1,9 @@
-def wsgi_application(environ, start_response):
-status = '200 OK'
-headers = [
-('Content-Type', 'text/plain')
-]
-S = environ['QUERY_STRING']
-n = S.rfind('?') + 1	
-body = S[n:len(S)]
-body = body.replace('&', '\n')
-start_response(status, headers )
-return [ body ]
+def appl(environ, start_response):
+	status = '200 OK'
+	headers = [('Content-Type', 'text/plain')]
+	S = environ['QUERY_STRING']
+	n = S.rfind('?') + 1	
+	body = S[n:len(S)]
+	body = body.replace('&', '\n')
+	start_response(status, headers )
+    return [ body ]
