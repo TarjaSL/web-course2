63e88e710237df8fb7fc912c0415d4b8b1abff33
Tarja <svetozar-vk@mail.ru>
2016-03-09 23:05:08 +0300

    Dynamic 6

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -45,7 +45,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
 )
 
 ROOT_URLCONF = 'ask.urls'
