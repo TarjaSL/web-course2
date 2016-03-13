584184c387d6bced4f663abcee433f0f2dae7c21
Tarja <svetozar-vk@mail.ru>
2016-03-13 15:58:20 +0300

    Creating DataBase

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Question(models.Model):
+	title = models.CharField(max_length=255)
+	text = models.TextField()
+	added_at = models.DateField()
+	rating = models.IntegerField()
+	author = models.ForeignKey(User)
+	likes = models.ManyToManyField(User, related_name='likes_set')
+	def __unicode__(self):
+		return self.title
+	def get_absolute_url(self):
+		return '/quest/%d/' % self.pk
+	class Meta:
+		db_table = 'question'		
+
+class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DateField()
+	question = models.OneToOneField(Question)
+	author = models.ForeignKey(User)
+	def __unicode__(self):
+		return self.title
+	def get_absolute_url(self):
+		return '/ans/%d/' % self.pk
+	class Meta:
+		db_table = 'answer'	
