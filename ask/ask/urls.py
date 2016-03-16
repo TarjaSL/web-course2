59cfefacfd8675e9d3491aae19bbf4df7260abb9
TarjaSL <svetozar-vk@mail.ru>
2016-03-16 16:09:08 +0300

    New try!

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^$', 'qa.views.test'),
+    url(r'^$', 'qa.views.new_quest'),
     url(r'^login/.*$', 'qa.views.test'),
     url(r'^signup/.*$', 'qa.views.test'),
-    url(r'^questions/\d+/$', 'qa.views.test'),
+    url(r'^question/(\d+)/$', 'qa.views.test'),
     url(r'^ask/.*$', 'qa.views.test'),
     url(r'^popular/.*$', 'qa.views.test'),
     url(r'^new/.*$', 'qa.views.test')
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+
 # Create your views here.
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
+	
+def new_quest(request):
+    questions = Question.objects.order_by('-id')
+    limit = 10
+    page = request.GET.get('page', 1)
+    paginator = Paginator(questions, limit)
+    paginator.baseurl = '/question/all_posts/?page='
+    page = paginator.page(page) # Page
+    return render(request, 'ask/quest_list.html', {
+        'questions': page.object_list,
+        'paginator': paginator, 'page': page,
+    })
+
+def popular_quest(request):
+    questions = Question.objects.order_by('-rating')
+    limit = 10
+    page = request.GET.get('page', 1)
+    paginator = Paginator(questions, limit)
+    paginator.baseurl = '/question/all_posts/?page='
+    page = paginator.page(page) # Page
+    return render(request, 'ask/popular_list.html', {
+        'questions': page.object_list,
+        'paginator': paginator, 'page': page,
+    })
+	
+def quest_details(request, pk):
+	try:
+		question = Question.objects.get(id=pk)
+	except question.DoesNotExist:
+		raise Http404
+	answers = question.answer_set.all()
+	return render(request, 'ask/quest_details.html', {'question': question,
+	'answers': answers,
+	})
+
+	
